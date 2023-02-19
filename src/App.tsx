55005e20918194bbaa1f8c68e9b527b0248d77c0
zaki <zaki20020302@gmail.com>
2023-02-19 09:54:20 +0900

    docs: #7 返却値の型指定

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Practice } from "./pratices/Practice";
+import { PracticeReturn } from "./pratices/PracticeReturn";
 
 export default function App() {
   return (
     <div>
       <Practice />
+      <PracticeReturn />
     </div>
   );
 }
