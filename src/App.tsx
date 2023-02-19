cd4514b8dbcbdc954871737be98e5109ad2f83e2
zaki <zaki20020302@gmail.com>
2023-02-19 10:20:15 +0900

    docs: #9 変数の型指定

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Practice } from "./pratices/Practice";
 import { PracticeReturn } from "./pratices/PracticeReturn";
+import { PracticeVariable } from "./pratices/PracticeVariable";
 
 export default function App() {
   return (
     <div>
       <Practice />
       <PracticeReturn />
+      <PracticeVariable />
     </div>
   );
 }
